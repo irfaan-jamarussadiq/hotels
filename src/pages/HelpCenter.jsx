4d2d084fab339402294b9b74ff5bbb3815f3cc4d
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2024-08-25 12:15:11 -0400

    Make sure image links display correctly on sub-pages

--- src/pages/HelpCenter.jsx
+++ src/pages/HelpCenter.jsx
@@ -8,11 +8,16 @@ function HelpCenter() {
       <div className='search-help'>
         <h2 className='subtitle'>Explore help articles</h2>
         <form className='search-dialog'>
-          <img className='search-icon' src='search.png' alt='Search Icon' />
+          <img className='search-icon' src='/search.png' alt='Search Icon' />
           <input className='search-help-input search-input' type='search' placeholder='Search help articles' />
         </form>
         <div className='help-section-cards'>
-          {helpSections.map(section => <HelpSectionCard key={section.section} helpSection={section} />)}
+          {helpSections.map(section => (
+            <HelpSectionCard
+              key={section.section}
+              helpSection={section}
+            />
+          ))}
         </div>
       </div>
     </div>
@@ -23,20 +28,20 @@ function HelpSectionCard({ helpSection }) {
   return (
     <div className='help-section-card'>
       <div className='help-section-header'>
-        <img className='help-section-icon' src={helpSection.icon} alt={helpSection.section} />
+        <img className='help-section-icon' src={`/${helpSection.icon}`} alt={helpSection.section} />
         <h3 className='help-section-title'>{helpSection.section}</h3>
       </div>
       <hr className='separator' />
       <ul className='help-links'>
         {helpSection.links.map(link => (
           <li key={link.href} className='help-link-item'>
-            <a className='help-link' href={link.href}>{link.description} &gt;</a>
+            <a className='help-link'>{link.description} &gt;</a>
           </li>
         ))}
       </ul>
       <hr className='separator' />
       <div className='more-options'>
-        <img className='more-icon' src='more.png' alt='More Options' />
+        <img className='more-icon' src='/more.png' alt='More Options' />
         <a className='help-link' href={helpSection.moreLink}>More &gt;</a>
       </div>
     </div>
