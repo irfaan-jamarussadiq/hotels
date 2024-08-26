5d2c5e833ecb8532ce089c7eff73f1a6b97d3681
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2024-08-25 21:04:53 -0400

    Implement modal backdrop styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,21 +6,23 @@ function Modal({ onClose, children }) {
   const title = location.state.title
 
   return (
-    <dialog className='modal' open>
-      <div className='modal-close'>
-        <button className='modal-close-btn' onClick={onClose}>
-          <span className='close-symbol'>✕</span>
-        </button>
-        <div className='explore-help-text'>Explore help articles</div>
+    <div className='dialog-wrapper'>
+      <div className='modal' id='dialog'>
+        <div className='modal-close'>
+          <button className='modal-close-btn' onClick={onClose}>
+            <span className='close-symbol'>✕</span>
+          </button>
+          <div className='explore-help-text'>Explore help articles</div>
+        </div>
+        <h1>{title}</h1>
+        {children}
+        <div className='modal-feedback'>
+          <h3>Was this helpful?</h3>
+          <img className='feedback-icon' src='/like.png' alt='Feedback was helpful' />
+          <img className='feedback-icon' src='/dislike.png' alt='Feedback was not helpful' />
+        </div>
       </div>
-      <h1>{title}</h1>
-      {children}
-      <div className='modal-feedback'>
-        <h3>Was this helpful?</h3>
-        <img className='feedback-icon' src='/like.png' alt='Feedback was helpful' />
-        <img className='feedback-icon' src='/dislike.png' alt='Feedback was not helpful' />
-      </div>
-    </dialog>
+    </div>
   )
 }
 
