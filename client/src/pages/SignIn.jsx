dbc034eaa7bd67f25dd1fe330c9b79aad15a7a75
Irfaan Jamarussadiq <irfaanj9@gmail.com>
2024-08-26 21:57:19 -0400

    Use Material UI theme for sign in component

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,19 +1,24 @@
 import './SignIn.css'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
 
 function SignIn() {
   return (
     <div className='SignIn'>
       <h1>Sign In</h1>
       <form className='sign-in-form'>
-        <div className='form-field'>
-          <label className='form-label' htmlFor='email'>Email</label>
-          <input className='form-input' id='email' type='email' placeholder='john.smith@example.com' />
-        </div>
-        <div className='form-field'>
-          <label className='form-label' htmlFor='password'>Password</label>
-          <input className='form-input' id='password' type='password' />
-        </div>
-        <button className='sign-in-btn'>Sign in</button>
+        <Grid container direction={"column"} spacing={2}>
+          <Grid item>
+            <TextField id='outlined-basic-email' fullWidth label='Email' variant='outlined' />
+          </Grid>
+          <Grid item>
+            <TextField id='outlined-basic-password' fullWidth type='password' label='Password' variant='outlined' />
+          </Grid>
+          <Grid item>
+            <Button variant="contained">Sign in</Button>
+          </Grid>
+        </Grid>
       </form>
     </div>
   )
